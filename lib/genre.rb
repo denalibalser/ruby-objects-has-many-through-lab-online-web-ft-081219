b2e444cbf3f93a3557a2faf5d9f8b7888d752382
Denali Balser <denali.balser167@gmail.com>
2019-09-01 16:13:28 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,13 +1,15 @@
 class Genre
+  attr_accessor :name, :artist
 
   @@all = []
 
-  def initialize
+  def initialize(name)
+    @name = name
     @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
 end
