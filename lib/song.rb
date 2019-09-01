0d444d7d5589b415208134461c0f2781af30c134
Denali Balser <denali.balser167@gmail.com>
2019-09-01 16:10:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,18 @@
 class Song
+  attr_accessor :name, :arist, :genre
 
   @@all = []
 
-  def initialize
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
     @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+
 end
