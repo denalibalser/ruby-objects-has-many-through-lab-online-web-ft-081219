17fd58d5704772e334ef258a0e29697604f63b80
Denali Balser <denali.balser167@gmail.com>
2019-09-01 16:40:56 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,18 +11,21 @@ class Doctor
     @@all
   end
 
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-  end
-
   def appointments
     Appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end 
 
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
 
   def patients
+    appointments.map do |appointment|
+      appointment.patient 
+    end 
+  end 
 
 
 end
